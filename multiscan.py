cba975bb210fc6dc6b30f62aee11183e0792ca94
Steve <steve@fam-reichel.de>
2017-02-23 08:36:19 +0100

    multiscan mit keyboard abbruch

--- multiscan.py
+++ multiscan.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 import socket
+import signal
+import sys
+from colorama import Fore, Style
+
+def signal_handler(signal, frame):
+  global interrupted
+  interrupted = True
+  print("bye")
+  sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 #serverliste einlesen
 f = open('./serverlist.sort', 'r')
@@ -8,25 +19,30 @@ list = lines.splitlines()
 ser = 0
 fai = 0
 
+inter = False
+
 def portscan(server, port):
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.settimeout(30)
+  sock.settimeout(2)
   try:
-    #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     conn = sock.connect((server,port))
-    #sock.close()
-    #return True
   except:
     return False
   sock.close()
   return True
 
+interrupted = False
+print("Abbruch mit Ctrl+C")
 for server in list:
   ser = ser + 1
   if portscan(server,80) or portscan(server,443):
-    print('Server', server, 'is open')
+    print(Fore.GREEN + 'Server', server, 'is open')
   else:
-    print('Server', server, 'is close')
+    print(Fore.RED + 'Server', server, 'is close')
     fai = fai + 1
+  if interrupted:
+    break
 
-print(ser, fai)
+
+print(Style.RESET_ALL)
+print("Tests:", ser, "Fehler:", fai)
